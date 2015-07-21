c573acef37dacceb291b96bdff56b52b832a3067
Ivan Shamatov <status.enable@gmail.com>
2015-07-21 13:53:08 +0300

    multiple files by carrierwave

--- lib/formtastic/helpers/file_column_detection.rb
+++ lib/formtastic/helpers/file_column_detection.rb
@@ -7,7 +7,8 @@ module Formtastic
         @files ||= {}
         @files[method] ||= (options[:as].present? && options[:as] == :file) || begin
           case file = @object.send(method) if @object && @object.respond_to?(method)
-          when file_methods.any?{|m| file.respond_to?(m)} then true
+          when file_methods.any?{|m| file.respond_to?(m)} 
+            true
           when file.is_a?(Array) && file_methods.any?{|m| file.first.respond_to?(m)}
             options[:input_html] ||= {}
             options[:input_html].merge(multiple: true)
